d750410ac349671efcfaca84e1e2f72cf914ac5a
aaalamandaardana <ardana1920@gmail.com>
2022-11-20 22:24:46 +0700

    Test Perubahan

--- src/components/BarNavigasi.jsx
+++ src/components/BarNavigasi.jsx
@@ -10,7 +10,7 @@ const BarNavigasi = () => {
             <nav className="navbar navbar-expand-lg bg-white py-3 shadow-sm">
             <div className="container">
                 <NavLink className="navbar-brand fw-bold fs-4" to="/beranda">
-                    One Mart
+                    One-Mart
                 </NavLink>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
